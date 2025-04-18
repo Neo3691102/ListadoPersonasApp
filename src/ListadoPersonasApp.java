3202c43bc80371609c120bec62c4a6352578874b
Neo3691102 <nestor8772@gmail.com>
2025-04-17 23:43:04 -0600

    Estructura de la aplicacion

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -11,7 +11,12 @@ public class ListadoPersonasApp {
         var salir = false;
         while(!salir){
             mostrarMenu();
-            salir = ejecutarOperacion(consola, personas);
+            try{
+                salir = ejecutarOperacion(consola, personas);
+            } catch(Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
+
             System.out.println();
         }
     }
@@ -24,5 +29,36 @@ public class ListadoPersonasApp {
                 3. Salir
                 """);
         System.out.print("Proporciona la opcion? ");
+    }
+
+    private static boolean ejecutarOperacion( Scanner console, List<Persona>personas){
+        var opcion = Integer.parseInt(console.nextLine());
+        var salir = false;
+
+        switch(opcion){
+            case 1 -> {
+                System.out.print("Proporciona el nombre: ");
+                var nombre = console.nextLine();
+                System.out.print("Proporciona el telefono: ");
+                var tel = console.nextLine();
+                System.out.print("Proporciona email: ");
+                var email = console.nextLine();
+                var persona = new Persona(nombre, tel, email);
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos");
+            }
+            case 2 -> {
+                System.out.println("Listado de personas: ");
+                //Mejora usando lamba y metodo de referencia
+//                personas.forEach((persona) -> System.out.println(persona)); funcion lamba
+                personas.forEach(System.out::println);//metodo de referencia
+            }
+            case 3 -> {
+                System.out.println("Hasta pronto...");
+                salir = true;
+            }
+            default -> System.out.println("Opcion erronea: " + opcion);
+        }
+        return salir;
     }
 }
